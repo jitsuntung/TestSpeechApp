5c33b72eae57be7b9895d7048293aa1398d120da
Jit Sun Tung <jitsuntung@gmail.com>
2015-10-22 10:35:05 -0500

    Grid add -v2

--- app/src/main/java/com/example/jit/testspeech/MainActivity.java
+++ app/src/main/java/com/example/jit/testspeech/MainActivity.java
@@ -37,7 +37,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (findViewById(R.id.fragment_container1) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            GridFragment firstFragment = new GridFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container1, firstFragment).commit();
+
     }
+
+
+}
 
     /**
      * Showing google speech input dialog
