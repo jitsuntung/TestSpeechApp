a8b9576decfb092ee8073613167101d9597c5cd7
Jit Sun Tung <jitsuntung@gmail.com>
2015-10-22 17:11:13 -0500

    TTT AI complete

---
+++ app/src/main/java/com/example/jit/testspeech/SingleGridFrag.java
@@ -0,0 +1,147 @@
+package com.example.jit.testspeech;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Created by jtung on 10/22/2015.
+ *
+ */
+public class SingleGridFrag extends Fragment {
+    static final String TAG = "taggy";
+    int[] arr = {0,0,0,0,0,0,0,0,0};
+    final Controller controller = new Controller();
+    int i;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        // get the photo size and spacing
+        // mPhotoSize = getResources().getDimensionPixelSize(R.dimen.photo_size);
+        //mPhotoSpacing = getResources().getDimensionPixelSize(R.dimen.photo_spacing);
+
+        //Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.gridfragment_layout, container, false);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(view.getContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if(arr[position]>0){
+                    Toast.makeText(getActivity(), "You Can't Click There!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    arr[position] = 1;
+                    i = controller.makeMove(position);
+                    Log.v(TAG, Integer.toString(i));
+
+                    if(i<0 || i>8){
+                        if(i>8){
+                            i=i-20;
+                            arr[i]=2;
+                            Toast.makeText(getActivity(), "You Lose!", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(i==-2){
+                            Toast.makeText(getActivity(), "It's a Draw!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    else{
+                        arr[i] = 2;
+                    }
+
+                    Fragment currentFragment = getFragmentManager().findFragmentByTag("SINGLEPEE");
+                    FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
+                    fragTransaction.detach(currentFragment);
+                    fragTransaction.attach(currentFragment);
+                    fragTransaction.commit();
+                }
+
+
+                /*This following code replaces the fragment, not what we want to do.
+                But put code here that you want to happen on selection of an item in the grid
+
+                FragmentTwo newFragment = new FragmentTwo();
+
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack
+                transaction.replace(R.id.fragment_place1, newFragment);
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();*/
+
+            }
+        });
+
+
+        return view;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return 9;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(251, 251));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                //imageView.setBackgroundResource(R.drawable.imageview_border);
+
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            if(arr[position]==0){
+                imageView.setImageResource(R.drawable.yellow_btn);
+            }
+            else if (arr[position]==1){
+                imageView.setImageResource(R.drawable.cross_btn);
+            }
+            else if(arr[position]==2){
+                imageView.setImageResource(R.drawable.circle_btn);
+            }
+
+
+            return imageView;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jit/testspeech/SinglePlayer.java
@@ -0,0 +1,50 @@
+package com.example.jit.testspeech;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SinglePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_player);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.singlep_frag_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            SingleGridFrag firstFragment = new SingleGridFrag();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(R.id.singlep_frag_container, firstFragment, "SINGLEPEE").commit();
+        }
+    }
+}
